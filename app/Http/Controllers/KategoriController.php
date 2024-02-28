45982ff330e36dcc4e7fedc35bcff1969652f9ea
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2024-02-28 12:11:10 +0700

    praktikum 5

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now(),
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode','SNK')->update(['kategori_nama' => 'Camilan']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' .$row.' baris';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode','SNK')->delete();
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' .$row.' baris';
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori',['data' => $data]);
+    }
+}
