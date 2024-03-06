2e581be692ceca3942eab2f388c83129e28d62e4
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2024-03-06 09:37:22 +0700

    praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
     public function index(){
         
-        $user = UserModel::where('username','manager9')->firstOrFail();
+        $user = UserModel::where('level_id',2)->count();
+        // dd($user);
         return view('user',['data' => $user]);
     }
 }
