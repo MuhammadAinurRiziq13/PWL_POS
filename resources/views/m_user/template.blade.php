fe84a23f51f7bae9e9c29a9434764135a90abbbd
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2024-03-25 20:11:00 +0700

    jobsheet 6

---
+++ resources/views/m_user/template.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CRUD Laravel</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+@yield('content')
+</div>
+</body>
+</html>
